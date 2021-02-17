545e154bc31f3e044a703fa29a322382bfb8ef4e
Daniel <33101553+djhayes1994@users.noreply.github.com>
2021-02-17 08:23:23 -0500

    Update MerakiVPNConfiguration.ps1
    Added DNSsuffix variable and added DNS suffix switch to Add-VPNConnection command.  Also fixed some of the grammar/formatting at the end of the script.

--- MerakiVPNConfiguration.ps1
+++ MerakiVPNConfiguration.ps1
@@ -1,19 +1,21 @@
 <#
 .SYNOPSIS
-  The purpose of this script is to minimize the ammount of work required to create a VPN connection on a Windows 7, 8, 8.1, or 10 workstation for Meraki VPN.
+  The purpose of this script is to minimize the ammount of work required to create a VPN connection on a Windows 8, 8.1, or 10 workstation for Meraki VPN.
 .DESCRIPTION
   Collects input from shell and inputs it into Add-VPNConnection command with the command structured for Meraki L2TP VPN connections.
 .INPUTS
   Name - This is the name of the VPN connection that will show within the Windows UI.
   ServerAddr - This is the server address which can be found either via the Meraki Dashboard or via CWM configurations, typically the configuration is named Remote Access.
   L2TPPSK - This is the preshared key for the L2TP connection. This can be found either via the Meraki Dashboard or via the customers configurations.
+  DnsSuffix - This is the dns suffix used by the VPN connection, for example: contoso.local
 .OUTPUTS
   Add-VPNConnection creates the VPN connection. 
 .NOTES
-  Version:        1.1
+  Version:        1.2
   Author:         Daniel Hayes
   Creation Date:  03/28/2019
-  Purpose/Change: Added pause at end of script so info could be read.
+  Updated: 02/07/2021
+  Purpose/Change: Added DNSsuffix variable and added DNS suffix switch to Add-VPNConnection command. 
   
 .EXAMPLE
   End user is requesting VPN access and the customer utilizes Meraki VPN. Meraki VPN does not have a client so it uses the Windows native VPN client. 
@@ -24,14 +26,15 @@
 $Name = Read-Host -Prompt 'Input name of VPN connection; Ie. Company VPN'
 $ServerAddr = Read-Host -Prompt 'Input server address can be WAN IP or Meraki Hostname'
 $L2TPPSK = Read-Host -Prompt 'Input Pre-shared key for VPN, can be found in Remote Access configuration'
+$DnsSuffix = Read-Host -Prompt 'Input the dns suffix used for the VPN connection'
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 Import-Module VpnClient
-Add-VpnConnection -RememberCredential -Name $Name -ServerAddress $ServerAddr -AuthenticationMethod Pap -TunnelType L2tp -EncryptionLevel Optional -L2tpPsk $L2TPPSK -Force
+Add-VpnConnection -RememberCredential -Name $Name -ServerAddress $ServerAddr -AuthenticationMethod Pap -TunnelType L2tp -EncryptionLevel Optional -L2tpPsk $L2TPPSK -DnsSuffix $DnsSuffix -Force
 Write-Host "VPN Connection has been created..."
-Write-Host "You named the connection:" $Name
-Write-Host "For the server address you entered:" $ServerAddr
-Write-Host "For the Pre-Shared key:" $L2TPPSK "was used."
+Write-Host "VPN Display Name:" $Name
+Write-Host "Server:" $ServerAddr
+Write-Host "Pre-Shared key:" $L2TPPSK "was used."
 Write-Host "Detailed information is located below...."
 Get-VpnConnection | fl
 Pause
