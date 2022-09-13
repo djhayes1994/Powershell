2a04c4bdfcb120a16e9a888a3aa6f258bf9780a8
Daniel <33101553+djhayes1994@users.noreply.github.com>
2022-09-13 14:39:37 -0400

    Initial Script Commit

---
+++ Get-UmbrellaDownloadLink.ps1
@@ -0,0 +1,7 @@
+#Example of download url: https://disthost.umbrella.com/roaming/upgrade/win/stage/RoamingClient_WIN_3.0.328.msi
+
+$baseurl = "https://disthost.umbrella.com/roaming/upgrade/win/stage/"
+$WebReq = Invoke-WebRequest -Uri "https://disthost.umbrella.com/roaming/upgrade/win/production/manifest.json" -UseBasicParsing | ConvertFrom-Json
+$FileName = $WebReq.downloadFilename
+$FullDownload = $baseurl+$FileName
+$FullDownload
