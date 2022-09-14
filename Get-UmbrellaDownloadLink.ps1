4fc7ac42a272703aa22eab01fcbff376341839da
Daniel <33101553+djhayes1994@users.noreply.github.com>
2022-09-14 09:34:01 -0400

    Fix url
    Fixed base url

--- Get-UmbrellaDownloadLink.ps1
+++ Get-UmbrellaDownloadLink.ps1
@@ -1,6 +1,6 @@
-#Example of download url: https://disthost.umbrella.com/roaming/upgrade/win/stage/RoamingClient_WIN_3.0.328.msi
+#Example: https://disthost.umbrella.com/roaming/upgrade/win/stage/RoamingClient_WIN_3.0.328.msi
 
-$baseurl = "https://disthost.umbrella.com/roaming/upgrade/win/stage/"
+$baseurl = "https://disthost.umbrella.com/roaming/upgrade/win/production/"
 $WebReq = Invoke-WebRequest -Uri "https://disthost.umbrella.com/roaming/upgrade/win/production/manifest.json" -UseBasicParsing | ConvertFrom-Json
 $FileName = $WebReq.downloadFilename
 $FullDownload = $baseurl+$FileName
